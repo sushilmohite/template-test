3cfba3e72b7a06fe96e2fffb79d6c18975737475
sushilmohite <work@Sushils-MacBook-Air.local>
2025-11-20 16:04:41 +0530

    Add variables

--- main.tf
+++ main.tf
@@ -4,7 +4,27 @@ terraform {
 
 provider "local" {}
 
+variable "file_content" {
+    type    = string
+    default = "Hello, World!"
+}
+
+variable "user_name" {
+    type    = string
+    default = "User"
+}
+
 resource "local_file" "hello_world" {
     filename = "${path.module}/files/helloworld.txt"
-    content  = "Hello, World!"
+    content  = var.file_content + " " + var.user_name
+}
+
+output "file_name_output" {
+    description = "Displaying file name"
+    value = local_file.hello_world.filename
+}
+
+output "file_output" {
+    description = "Displaying file content"
+    value = local_file.hello_world.content
 }
